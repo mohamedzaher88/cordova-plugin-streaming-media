3c494207cb08f8eede40d4fe8d9cd99f435eda17
mohamedzaher88 <mohamedzaher88@gmail.com>
2017-04-03 02:42:18 +0200

    Add files via upload

--- src/ios/startapp.m
+++ src/ios/startapp.m
@@ -1,18 +1,127 @@
-/*
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
+#import "startapp.h"
 
- http://www.apache.org/licenses/LICENSE-2.0
+@implementation startapp
 
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
- */
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    /*
+     Init of the startapp interstitials
+     NOTE: replace the ApplicationID and the PublisherID with your own IDs
+     */
+    STAStartAppSDK* sdk = [STAStartAppSDK sharedInstance];
+    sdk.devID = @"107816600";
+    sdk.appID = @"204613054";
+    
+      STASplashPreferences *splashPreferences = [[STASplashPreferences alloc] init];
+      splashPreferences.splashMode = STASplashModeTemplate;
+       splashPreferences.splashTemplateTheme = STASplashTemplateThemeOcean;
+       splashPreferences.splashLoadingIndicatorType = STASplashLoadingIndicatorTypeDots;
+       splashPreferences.splashTemplateIconImageName = @"icon";
+       splashPreferences.splashTemplateAppName = @"Smarts El-Wady Games";
+    
+      [sdk showSplashAdWithPreferences:splashPreferences];
+    
+    
+    startAppAd_autoLoad = [[STAStartAppAd alloc] init];
+    startAppAd_loadShow = [[STAStartAppAd alloc] init];
+    
+}
+
+
+
+
+#pragma mark View lifecycle
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    // View defaults to full size.  If you want to customize the view's size, or its subviews (e.g. webView),
+    // you can do so here.
+    
+    [super viewWillAppear:animated];
+    
+    // loading the StartApp Ad
+    [startAppAd_autoLoad loadAdWithDelegate:self];
+    
+    
+    /*
+     
+     
+     if (startAppBanner_auto == nil) {
+     startAppBanner_auto = [[STABannerView alloc] initWithSize:STA_AutoAdSize
+     autoOrigin:STAAdOrigin_Bottom
+     withView:self.view withDelegate:nil];
+     [self.view addSubview:startAppBanner_auto];
+     }
+    
+     
+     
+     */
+
+    
+    
+}
+
+- (void) didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
+    
+    // notify StartApp auto Banner orientation change
+    [startAppBanner_auto didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    
+    // notify StartApp fixed position Banner orientation change
+    [startAppBanner_fixed didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    
+    [super didRotateFromInterfaceOrientation:fromInterfaceOrientation ];
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskAll;
+}
+
+// Tell the system It should autorotate
+- (BOOL) shouldAutorotate{
+    return YES && (startAppAd_loadShow.STAShouldAutoRotate || startAppAd_autoLoad.STAShouldAutoRotate);
+}
+
+// StartApp Ad loaded successfully
+- (void) didLoadAd:(STAAbstractAd*)ad;
+{
+    NSLog(@"StartApp Ad had been loaded successfully");
+    
+    // Show the Ad
+    if (startAppAd_loadShow == ad) {
+        [startAppAd_loadShow showAd];
+    }
+}
+
+// StartApp Ad failed to load
+- (void) failedLoadAd:(STAAbstractAd*)ad withError:(NSError *)error;
+{
+    NSLog(@"StartApp Ad had failed to load");
+}
+
+// StartApp Ad is being displayed
+- (void) didShowAd:(STAAbstractAd*)ad;
+{
+    NSLog(@"StartApp Ad is being displayed");
+}
+
+// StartApp Ad failed to display
+- (void) failedShowAd:(STAAbstractAd*)ad withError:(NSError *)error;
+{
+    
+    NSLog(@"StartApp Ad is failed to display");
+    if (startAppAd_autoLoad == ad) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Can't show ad" message:@"Ad is not loaded yet, please try again" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alertView show];
+    }
+}
+
+- (void) didCloseAd:(STAAbstractAd*)ad {
+    if (startAppAd_autoLoad == ad) {
+        [startAppAd_autoLoad loadAd:STAAdType_Automatic withDelegate:self];
+    }
+}
+
