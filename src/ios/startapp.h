cdbbfdd986ba8424931271aa6d7a396d39aa69e7
mohamedzaher88 <mohamedzaher88@gmail.com>
2017-04-03 02:06:02 +0200

    Add files via upload

--- src/ios/startapp.h
+++ src/ios/startapp.h
@@ -1,13 +1,3 @@
 #import <UIKit/UIKit.h>
 #import <StartApp/StartApp.h>
 
-@interface startapp : CDVPlugin <STADelegateProtocol>
-{
-    
-STAStartAppAd *startAppAd_autoLoad;
-STAStartAppAd *startAppAd_loadShow;
-STABannerView *startAppBanner_auto;
-STABannerView *startAppBanner_fixed;
-    
-}
-@end
